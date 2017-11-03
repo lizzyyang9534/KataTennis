327a1ae913857c99e921c4ee0a7733972f5fba17
TITANSOFT\lizzy.yang71 <lizzy.yang@titansoft.com.tw>
2017-11-03 17:44:24 +0800

    left advantage case

---
+++ KataTennis/Tennis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTennis
+{
+    public class Tennis
+    {
+        public static string GetScore(int playerOneScore, int playerTwoScore)
+        {
+            var scoreDic = new Dictionary<int, string>()
+            {
+                { 0, "Love" },
+                { 15, "Fifteen" },
+                { 30, "Thirty" },
+                { 40, "Forty" }
+            };
+
+            if (playerOneScore + playerTwoScore == 0)
+                return "Game Start";
+
+            if (playerOneScore == playerTwoScore)
+                return "Deuce";
+
+            return scoreDic[playerOneScore] + " " + scoreDic[playerTwoScore];
+        }
+    }
+}
---
+++ KataTennis/TennisTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTennis
+{
+    [TestClass]
+    public class TennisTest
+    {
+        [TestMethod]
+        public void TestGameStart()
+        {
+            string actual = Tennis.GetScore(0, 0);
+            Assert.AreEqual("Game Start", actual);
+        }
+
+        [TestMethod]
+        public void TestLoveFifteen()
+        {
+            string actual = Tennis.GetScore(0, 15);
+            Assert.AreEqual("Love Fifteen", actual);
+        }
+
+        [TestMethod]
+        public void TestLoveThirty()
+        {
+            string actual = Tennis.GetScore(0, 30);
+            Assert.AreEqual("Love Thirty", actual);
+        }
+
+        [TestMethod]
+        public void TestLoveForty()
+        {
+            string actual = Tennis.GetScore(0, 40);
+            Assert.AreEqual("Love Forty", actual);
+        }
+
+        [TestMethod]
+        public void TestFifteenLove()
+        {
+            string actual = Tennis.GetScore(15, 0);
+            Assert.AreEqual("Fifteen Love", actual);
+        }
+
+        [TestMethod]
+        public void TestDeuce()
+        {
+            string actual = Tennis.GetScore(40, 40);
+            Assert.AreEqual("Deuce", actual);
+        }
+
+        [TestMethod]
+        public void TestAdvantage()
+        {
+            string actual = Tennis.GetScore(40, 30);
+            Assert.AreEqual("Advantage", actual);
+        }
+
+    }
+}
