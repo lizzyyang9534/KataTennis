85bef06864d25efbacb19def86d808748156036c
TITANSOFT\lizzy.yang71 <lizzy.yang@titansoft.com.tw>
2017-11-11 23:31:50 +0800

    complete

--- KataTennis/Tennis.cs
+++ KataTennis/Tennis.cs
@@ -10,20 +10,50 @@ namespace KataTennis
     {
         public static string GetScore(int playerOneScore, int playerTwoScore)
         {
+            if (IsValidInput(playerOneScore, playerTwoScore))
+                return "Invalid input";
+
+            if (IsDeuce(playerOneScore, playerTwoScore))
+                return "Deuce";
+
+            if (IsAdvantage(playerOneScore, playerTwoScore))
+                return "Advantage";
+
+            if (HasWinner(playerOneScore, playerTwoScore))
+                return Math.Max(playerOneScore, playerTwoScore) == playerOneScore ? "Player1 Win" : "Player2 Win";
+
+            return GetTwoPlayerScore(playerOneScore, playerTwoScore);
+        }
+
+        private static bool IsValidInput(int playerOneScore, int playerTwoScore)
+        {
+            return playerOneScore + playerTwoScore < 0;
+        }
+
+        private static bool IsDeuce(int playerOneScore, int playerTwoScore)
+        {
+            return (playerOneScore == playerTwoScore) && (playerOneScore + playerTwoScore > 0);
+        }
+
+        private static bool IsAdvantage(int playerOneScore, int playerTwoScore)
+        {
+            return playerOneScore + playerTwoScore > 4 && Math.Abs(playerOneScore - playerTwoScore) == 1;
+        }
+
+        private static bool HasWinner(int playerOneScore, int playerTwoScore)
+        {
+            return playerOneScore + playerTwoScore > 4 && Math.Abs(playerOneScore - playerTwoScore) > 1;
+        }
+
+        private static string GetTwoPlayerScore(int playerOneScore, int playerTwoScore)
+        {
             var scoreDic = new Dictionary<int, string>()
             {
                 { 0, "Love" },
-                { 15, "Fifteen" },
-                { 30, "Thirty" },
-                { 40, "Forty" }
+                { 1, "Fifteen" },
+                { 2, "Thirty" },
+                { 3, "Forty" }
             };
-
-            if (playerOneScore + playerTwoScore == 0)
-                return "Game Start";
-
-            if (playerOneScore == playerTwoScore)
-                return "Deuce";
-
             return scoreDic[playerOneScore] + " " + scoreDic[playerTwoScore];
         }
     }
--- KataTennis/TennisTest.cs
+++ KataTennis/TennisTest.cs
@@ -7,52 +7,151 @@ namespace KataTennis
     public class TennisTest
     {
         [TestMethod]
-        public void TestGameStart()
+        public void TestLoveLove()
         {
             string actual = Tennis.GetScore(0, 0);
-            Assert.AreEqual("Game Start", actual);
+            Assert.AreEqual("Love Love", actual);
         }
 
         [TestMethod]
         public void TestLoveFifteen()
         {
-            string actual = Tennis.GetScore(0, 15);
+            string actual = Tennis.GetScore(0, 1);
             Assert.AreEqual("Love Fifteen", actual);
         }
 
         [TestMethod]
         public void TestLoveThirty()
         {
-            string actual = Tennis.GetScore(0, 30);
+            string actual = Tennis.GetScore(0, 2);
             Assert.AreEqual("Love Thirty", actual);
         }
 
         [TestMethod]
         public void TestLoveForty()
         {
-            string actual = Tennis.GetScore(0, 40);
+            string actual = Tennis.GetScore(0, 3);
             Assert.AreEqual("Love Forty", actual);
         }
 
         [TestMethod]
         public void TestFifteenLove()
         {
-            string actual = Tennis.GetScore(15, 0);
+            string actual = Tennis.GetScore(1, 0);
             Assert.AreEqual("Fifteen Love", actual);
         }
 
         [TestMethod]
-        public void TestDeuce()
+        public void TestDeuceOn3_3()
         {
-            string actual = Tennis.GetScore(40, 40);
+            string actual = Tennis.GetScore(3, 3);
             Assert.AreEqual("Deuce", actual);
         }
 
         [TestMethod]
-        public void TestAdvantage()
+        public void TestDeuceOn4_4()
         {
-            string actual = Tennis.GetScore(40, 30);
+            string actual = Tennis.GetScore(4, 4);
+            Assert.AreEqual("Deuce", actual);
+        }
+
+        [TestMethod]
+        public void TestDeuceOn5_5()
+        {
+            string actual = Tennis.GetScore(5, 5);
+            Assert.AreEqual("Deuce", actual);
+        }
+
+        [TestMethod]
+        public void TestAdvantageOn3_2()
+        {
+            string actual = Tennis.GetScore(3, 2);
             Assert.AreEqual("Advantage", actual);
+        }
+
+        [TestMethod]
+        public void TestAdvantageOn3_4()
+        {
+            string actual = Tennis.GetScore(3, 4);
+            Assert.AreEqual("Advantage", actual);
+        }
+
+        [TestMethod]
+        public void TestAdvantageOn4_5()
+        {
+            string actual = Tennis.GetScore(4, 5);
+            Assert.AreEqual("Advantage", actual);
+        }
+
+        [TestMethod]
+        public void TestAdvantageOn5_6()
+        {
+            string actual = Tennis.GetScore(5, 6);
+            Assert.AreEqual("Advantage", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerOneWinOn4_1()
+        {
+            string actual = Tennis.GetScore(4, 1);
+            Assert.AreEqual("Player1 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerOneWinOn4_2()
+        {
+            string actual = Tennis.GetScore(4, 2);
+            Assert.AreEqual("Player1 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerOneWinOn5_3()
+        {
+            string actual = Tennis.GetScore(5, 3);
+            Assert.AreEqual("Player1 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerOneWinOn6_4()
+        {
+            string actual = Tennis.GetScore(6, 4);
+            Assert.AreEqual("Player1 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerTwoWinOn1_4()
+        {
+            string actual = Tennis.GetScore(1, 4);
+            Assert.AreEqual("Player2 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerTwoWinOn2_4()
+        {
+            string actual = Tennis.GetScore(2, 4);
+            Assert.AreEqual("Player2 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerTwoWinOn3_5()
+        {
+            string actual = Tennis.GetScore(3, 5);
+            Assert.AreEqual("Player2 Win", actual);
+        }
+
+        [TestMethod]
+        public void TestPlayerTwoWinOn4_6()
+        {
+            string actual = Tennis.GetScore(4, 6);
+            Assert.AreEqual("Player2 Win", actual);
+        }
+
+
+        [TestMethod]
+        public void TestInvalidInput()
+        {
+            string actual = Tennis.GetScore(-1, -2);
+            Assert.AreEqual("Invalid input", actual);
         }
 
     }
